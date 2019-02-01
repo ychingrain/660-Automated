2fc0969424725e1ed29f2f86947d2b54cd86b0af
Renato Mikio Nakagomi <rmikio@gmail.com>
2019-02-02 07:28:34 +1100

    Replaced hard coded 3 times by while loop

--- ConverTemp.py
+++ ConverTemp.py
@@ -5,8 +5,21 @@ def doConversion():
     tempInFAsString = input('Enter the temperature in Fahrenheit: ')
     tempInF = int( tempInFAsString )
     tempInC = round(((tempInF - 32) * 5/9),1)
-    print('The temperature in Celsius is', tempInC, 'degrees')
+    print("The temperature in Celsius is %f degrees." %(tempInC))
 
-for conversionCount in range( 3 ):
+yesplease = 1
+response = ""
+while yesplease:
     doConversion()
+    response = raw_input('Do you want to convert another temperature (y/n)? ')
+#    print("Response = %s" % format(response))
+    while response != "y" and response != "n": 
+        response = raw_input('Do you want to convert another temperature (y/n)?')
+    if response == "y":
+        yesplease = 1
+    else:
+        print("Bye!")
+        yesplease = 0
+
+
 
